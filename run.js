4514aff97cde49e6ff2652f0bd2162d5de597e47
Gary Liu (Wicresoft) <v-guiliu@microsoft.com>
2017-01-31 17:15:17 +0800

    update driver

--- run.js
+++ run.js
@@ -1,38 +1,67 @@
 var sql = require('mssql'),
-    fs = require('fs')
+    fs = require('fs');
+require('msnodesqlv8');
+// var config = {
+//     user: 'gary',
+//     password: 'QWEasdzxc123',
+//     server: 'garysql.database.windows.net',
+//     database: 'garytestsql',
+//     requestTimeout:300000,
+//     options: {
+//         encrypt: true
+//     }
+// };
 
 var config = {
-    user: 'gary',
-    password: 'QWEasdzxc123',
-    server: 'garysql.database.windows.net',
-    database: 'garytestsql',
-    requestTimeout:300000,
+    server: '10.168.177.44',
+    database: 'StackOverFlowDevDB',
+    driver: 'msnodesqlv8',
+    requestTimeout: 300000,
     options: {
-        encrypt: true
+        trustedConnection: true
     }
 };
 
-// let connection = new sql.Connection(config);
+let connection = new sql.Connection(config);
 
+connection.connect().then(() => {
+    let sentence = fs.readFileSync('./sql/sql/getEngineerMetrics.sql');
+    // console.log(sentence.toString());
+    let request = new sql.Request(connection);
+
+    //use stmt 
+    request.query(sentence.toString()).then((data)=>{
+        console.dir(data)
+        connection.close();
+    }).catch((err)=>{
+        console.error(err);
+    })
+
+})
+
+//Stored Procedure
 // connection.connect().then(() => {
-//     let sentence = fs.readFileSync('user-tags-vol.sql');
-//     // console.log(sentence.toString());
 //     let request = new sql.Request(connection);
 
-//     //use stmt 
-//     request.query(sentence.toString()).then((data)=>{
-//         console.dir(data)
-//         Connection.close();
-//     }).catch((err)=>{
-//         console.error(err);
-//     })
+//     request.input('alias', sql.NVarChar, 'v-guiliu')
+//         .input('start', sql.NVarChar, '2017-01-02')
+//         .input('end', sql.NVarChar, '2017-01-02')
+//         // .output('return_value', sql.Int)
+//         .execute('getEngineerMetrics').then((data) => {
+//             console.dir(data)
+//             connection.close();
+//         }).catch((err) => {
+//             console.error(err);
+//         })
 
+// }).catch((err) => {
+//     console.error(err);
 // })
 
-var SqlUtility = require('./sql_models/SqlUtility.js');
-// console.log(SqlUtility)
-// console.log(SqlUtility.parseUTCtodatetime('Question.creation_date'));
+// var SqlUtility = require('./sql_models/SqlUtility.js');
+// // console.log(SqlUtility)
+// // console.log(SqlUtility.parseUTCtodatetime('Question.creation_date'));
 
-var sqlDriver = require('./sql_models/SqlDriver.js');
-var question = new sqlDriver('Question');
-console.log(question.table)
+// var sqlDriver = require('./sql_models/SqlDriver.js');
+// var question = new sqlDriver('Question');
+// console.log(question.table)
---
+++ sql/sql/getEngineerMetrics.sql
@@ -0,0 +1,4 @@
+EXEC	[dbo].[getEngineerMetrics]
+		@alias = N'v-guiliu',
+		@start = N'2017-01-01',
+		@end = N'2017-01-01'
